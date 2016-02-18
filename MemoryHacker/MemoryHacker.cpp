a94fff6ba20897f593d3dfd9a48d44f9800f528c
yiikai <yiikai1987910@gmail.com>
2016-02-18 16:01:05 +0800

    modify a little

--- MemoryHacker/MemoryHacker.cpp
+++ MemoryHacker/MemoryHacker.cpp
@@ -47,18 +47,22 @@ public:
 };
 
 
-
 int _tmain(int argc, _TCHAR* argv[])
 {
 	hackerRBTree<int, int,less<int>> tree;
-	tree.insert_equal(1,10);
-	tree.insert_equal(2, 9);
-	tree.insert_equal(3, 8);
-	tree.insert_equal(4, 7);
-	tree.insert_equal(5, 6);
-	tree.insert_equal(6, 5);
-	tree.insert_equal(7, 4);
-	tree.insert_equal(8, 3);
+	tree.insert_unequal(1,10);
+	tree.insert_unequal(2, 9);
+	tree.insert_unequal(3, 8);
+	tree.insert_unequal(4, 7);
+	tree.insert_unequal(5, 6);
+	tree.insert_unequal(6, 5);
+	tree.insert_unequal(7, 4);
+	tree.insert_unequal(8, 3);
+	printf("size is %d\n", tree.size());
+	hackerRBTree<int, int, less<int>>::iterator itr;
+	itr = tree.findByKey(6);
+	tree.remove_node(itr);
+	printf("size is %d\n", tree.size());
 	return 0;  
 }
 
